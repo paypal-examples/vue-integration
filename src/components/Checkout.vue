220030dec32537280578328adf9d4ac6064b1113
Amy Egan <amyegan@paypal.com>
2021-05-25 14:48:38 -0400

    Add createOrder and onApprove

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -3,24 +3,49 @@
 </template>
 
 <script>
-
 export default {
-  name: 'Checkout',
-  beforeCreate: function() {
-    this.$loadScript({'client-id': CLIENT_ID})
-      .then(paypal => {
-        paypal.Buttons().render('#paypal-button-container')
+  name: "Checkout",
+  beforeCreate: function () {
+    this.$loadScript({ "client-id": CLIENT_ID }).then((paypal) => {
+      paypal
+        .Buttons({
+          createOrder: this.createOrder,
+          onApprove: this.onApprove,
+        })
+        .render("#paypal-button-container");
+    });
+  },
+  props: {
+    cartTotal: {
+      type: Number,
+      default: 0.01,
+    },
+  },
+  methods: {
+    createOrder: function (data, actions) {
+      console.log("Creating order...");
+      return actions.order.create({
+        purchase_units: [
+          {
+            amount: {
+              value: this.cartTotal,
+            },
+          },
+        ],
       });
-  }
-}
+    },
+    onApprove: function (data, actions) {
+      console.log("Order approved...");
+      return actions.order.capture().then(console.log("Order complete!"));
+    },
+  },
+};
 
-const CLIENT_ID = 'test';
-
-
+const CLIENT_ID = "test";
 </script>
 
 <style>
-  #paypal-button-container {
-    margin: 30px 0;
-  }
+#paypal-button-container {
+  margin: 30px 0;
+}
 </style>
