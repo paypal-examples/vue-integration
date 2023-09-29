87889ccddcef2be88bc4c556405445c0678136e4
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-09-29 16:29:40 +0100

    feat: migrate Checkout to script setup

---
+++ src/components/CheckoutPayment.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { loadScript } from '@paypal/paypal-js';
+import { onBeforeMount, ref } from 'vue'
+const { cartTotal } = defineProps({
+  cartTotal: {
+    type: Number,
+    default: 0.01,
+  }
+})
+const CLIENT_ID = 'test';
+const paid  = ref(false)
+
+onBeforeMount(function() {
+    loadScript({ 'client-id': CLIENT_ID }).then((paypal) => {
+      paypal
+        .Buttons({
+          createOrder: createOrder,
+          onApprove: onApprove,
+        })
+        .render('#paypal-button-container');
+    });
+  })
+
+   function createOrder(data, actions) {
+      console.log('Creating order...');
+      return actions.order.create({
+        purchase_units: [
+          {
+            amount: {
+              value: cartTotal,
+            },
+          },
+        ],
+      });
+    }
+
+function onApprove(data, actions) {
+  console.log('Order approved...');
+  return actions.order.capture().then(() => {
+    this.paid = true;
+    console.log('Order complete!');
+  });
+}
+</script>
+
+<template>
+  <div v-if="!paid" id="paypal-button-container"></div>
+  <div v-else id="confirmation">Order complete!</div>
+</template>
+
+<style>
+#paypal-button-container {
+  margin: 30px 0;
+}
+
+#confirmation {
+  color: green;
+  margin-top: 1em;
+  font-size: 2em;
+}
+</style>
